eceb5547c85c1f1207718e7bf69cc798a2918166
Jeff Wright <jeffw387@gmail.com>
2019-03-03 08:29:50 -0800

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 use chrono::{
     self,
     NaiveDate,
+    NaiveTime
 };
 
-use sched;
-use sched::models::Shift;
-
 fn main() {
-    let s1_start =
-        NaiveDate::from_ymd(2019, 3, 23).and_hms(7, 0, 0);
+    let s1_start_date =
+        NaiveDate::from_ymd(2019, 3, 23);
+    let s1_start_time = NaiveTime::from_hms(7, 0, 0);
     let s1_dur = chrono::Duration::hours(12);
-    let s1 = Shift::new(s1_start, s1_dur);
     println!(
         "The shift starts at {} and lasts {} hours.",
-        s1.start,
-        s1.duration.num_hours()
+        s1_start_date.and_time(s1_start_time),
+        s1_dur.num_hours()
     );
 }
