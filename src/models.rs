b3bd009a75819471cefc2752b7fe923f02f5b068
Jeff Wright <jeffw387@gmail.com>
2019-03-02 20:59:52 -0800

    new shift struct

--- src/models.rs
+++ src/models.rs
@@ -6,17 +6,24 @@ use chrono::{
 };
 use diesel::deserialize::Queryable;
 
-pub struct Shift {
-    pub start: NaiveDateTime,
-    pub duration: Duration,
+#[derive(Clone, Debug, Insertable)]
+#[table_name = "shifts"]
+pub struct NewShift {
+    pub employee_id: i32,
+    pub start_date: NaiveDate,
+    pub start_time: NaiveTime,
+    pub duration_hours: f32,
 }
 
-impl Shift {
-    pub fn new(
-        start: NaiveDateTime,
-        duration: Duration,
-    ) -> Shift {
-        Shift { start, duration }
+impl NewShift {
+    pub fn new(employee: &Employee, start_date: NaiveDate, start_time: NaiveTime, duration_hours: f32) -> NewShift {
+        NewShift {
+            employee_id: employee.id,
+            start_date,
+            start_time,
+            duration_hours
+        }
+    }
     }
 }
 
