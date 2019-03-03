e91f3ba890695dda3f5f2bb169f8b87510c995b6
Jeff Wright <jeffw387@gmail.com>
2019-03-02 20:58:16 -0800

    updated schema

--- src/schema.rs
+++ src/schema.rs
@@ -6,3 +6,18 @@ table! {
         phone_number -> Nullable<Varchar>,
     }
 }
+
+table! {
+    shifts (id) {
+        id -> Int4,
+        employee_id -> Int4,
+        start_date -> Date,
+        start_time -> Time,
+        duration_hours -> Float4,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    employees,
+    shifts,
+);
