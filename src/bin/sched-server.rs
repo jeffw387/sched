8e5789228b10b25505fb1ea64604cfce72dceb63
Jeff Wright <jeffw387@gmail.com>
2019-03-07 20:13:33 -0800

    basic sched server stub

--- src/bin/sched-server.rs
+++ src/bin/sched-server.rs
@@ -1,5 +1,10 @@
-use actix_web::server;
+use actix_web::{server, App};
 
 fn main() {
-    
+    let _ = server::HttpServer::new(
+        || App::new()
+            .resource("/sched", |r| { r.f(|_| "Scheduler stub"); }))
+            .bind("localhost:8080")
+            .unwrap()
+            .run();
 }
