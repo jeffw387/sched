23727dca3e71ff9109615c75b2a4f532dbac513b
Jeff Wright <jeffw387@gmail.com>
2019-03-15 08:52:53 -0700

    get all employees api

--- sched/src/api.rs
+++ sched/src/api.rs
@@ -1,3 +1,4 @@
-pub const API_LOGIN: &str = "/sched/login";
 pub const API_INDEX: &str = "/sched";
+pub const API_LOGIN: &str = "/sched/login";
+pub const API_GET_EMPLOYEES: &str = "/sched/get_employees";
 pub const API_GET_SHIFTS: &str = "/sched/get_shifts";
