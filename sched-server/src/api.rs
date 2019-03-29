f93be2f6bf8a66b306b25667153d863ce8829549
Jeff Wright <jeffw387@gmail.com>
2019-03-29 09:34:33 -0700

    api update

---
+++ sched-server/src/api.rs
@@ -0,0 +1,16 @@
+pub const API_INDEX: &str = "/sched";
+pub const API_ADD_USER: &str = "/sched/add_user";
+pub const API_CHANGE_PASSWORD: &str = "/sched/change_password";
+pub const API_REMOVE_USER: &str = "/sched/remove_user";
+pub const API_LOGIN: &str = "/sched/login_request";
+pub const API_GET_SETTINGS: &str = "/sched/get_settings";
+pub const API_ADD_SETTINGS: &str = "/sched/add_settings";
+pub const API_REPLACE_SETTINGS: &str = "/sched/replace_settings";
+pub const API_GET_EMPLOYEES: &str = "/sched/get_employees";
+pub const API_ADD_EMPLOYEE: &str = "/sched/add_employee";
+pub const API_REPLACE_EMPLOYEE: &str = "/sched/replace_employee";
+pub const API_REMOVE_EMPLOYEE: &str = "/sched/remove_employee";
+pub const API_GET_SHIFTS: &str = "/sched/get_shifts";
+pub const API_ADD_SHIFT: &str = "/sched/add_shift";
+pub const API_REPLACE_SHIFT: &str = "/sched/replace_shift";
+pub const API_REMOVE_SHIFT: &str = "/sched/remove_shift";
