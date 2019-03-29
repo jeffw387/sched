023865422fb5ab78424f6b07877564b4d10190ae
Jeff Wright <jeffw387@gmail.com>
2019-03-29 09:35:54 -0700

    database migration settings_table_create

---
+++ sched-server/migrations/2019-03-29-154750_settings_table/down.sql
@@ -0,0 +1,6 @@
+ALTER TABLE users DROP COLUMN startup_settings;
+DROP TABLE per_employee_settings;
+DROP TABLE settings;
+DROP TYPE LastNameStyle;
+DROP TYPE HourFormat;
+DROP TYPE ViewType;
---
+++ sched-server/migrations/2019-03-29-154750_settings_table/src/schema.rs
@@ -0,0 +1,68 @@
+table! {
+    employees (id) {
+        id -> Int4,
+        first -> Varchar,
+        last -> Varchar,
+        phone_number -> Nullable<Varchar>,
+    }
+}
+
+table! {
+    per_employee_settings (id) {
+        id -> Int4,
+        settings_id -> Int4,
+        employee_id -> Int4,
+        red -> Float4,
+        green -> Float4,
+        blue -> Float4,
+        alpha -> Float4,
+    }
+}
+
+table! {
+    sessions (token) {
+        token -> Text,
+    }
+}
+
+table! {
+    settings (id) {
+        id -> Int4,
+        user_id -> Int4,
+        view_type -> Viewtype,
+        hour_format -> Hourformat,
+        last_name_style -> Lastnamestyle,
+    }
+}
+
+table! {
+    shifts (id) {
+        id -> Int4,
+        employee_id -> Int4,
+        year -> Int4,
+        month -> Int4,
+        day -> Int4,
+        hour -> Int4,
+        minute -> Int4,
+        hours -> Int4,
+        minutes -> Int4,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Int4,
+        email -> Varchar,
+        password_hash -> Varchar,
+        startup_settings -> Nullable<Int4>,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    employees,
+    per_employee_settings,
+    sessions,
+    settings,
+    shifts,
+    users,
+);
