a7037814301779720a94877b419ce4dc77992a18
Jeff Wright <jeffw387@gmail.com>
2019-04-16 15:52:50 -0700

    white color

--- sched-client-elm/src/Montholith.elm
+++ sched-client-elm/src/Montholith.elm
@@ -1237,6 +1237,7 @@ defaultShadow =
     }
 
 black = Element.rgb 0 0 0
+white = Element.rgb 1 1 1
 lightGreen = Element.rgb 0.65 0.85 0.65
 borderColor = Element.rgb 0.7 0.7 0.7
 shiftModalElement : Model -> ShiftModalData -> Element Message
