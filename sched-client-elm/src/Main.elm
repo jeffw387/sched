c00c6acf968af6ee7b318deef049cce9d2f2e47a
Jeff Wright <jeffw387@gmail.com>
2019-04-24 10:59:44 -0700

    UpdateViewName

--- sched-client-elm/src/Main.elm
+++ sched-client-elm/src/Main.elm
@@ -1044,6 +1044,15 @@ update message model =
       (model, Cmd.none)
         
   -- View edit messages
+    (CalendarPage page, UpdateViewName name) ->
+      case (page.modal, getActiveSettings model) of 
+        (ViewEditModal _, Just active) ->
+          let
+            settings = active.settings
+            updatedSettings = { settings | name = name }
+          in
+            (model, updateSettings updatedSettings)
+        _ -> (model, Cmd.none)
     (CalendarPage page, UpdateViewType viewType) ->
       case (page.modal, getActiveSettings model) of 
         (ViewEditModal _, Just active) ->
